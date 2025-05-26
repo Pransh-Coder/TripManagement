f600c1c77462f8d1e011d34e2322e34b419b4854
Pransh_Rastogi <rastogipransh4@gmail.com>
2025-05-26 15:18:32 +0530

    minor code refactoring

--- src/main/kotlin/TripRepository.kt
+++ src/main/kotlin/TripRepository.kt
@@ -21,17 +21,6 @@ class TripRepository {
         tripList.add(tripDetail)
     }
 
-    fun deleteTrip(id: Int){
-        val tripData = tripList.find { it.id == id }
-        if (tripList.isNotEmpty()){
-            val isSuccess = tripList.remove(tripData)
-            println("Trip Deletion Success: $isSuccess")
-        }
-        else{
-            throw Exception("No trips exists")
-        }
-    }
-
     fun searchTripBySourceDestination(searchQuery: SearchQuery){
         if (tripList.isNotEmpty()){
             val tripSearchResults = tripList.filter {
@@ -45,17 +34,18 @@ class TripRepository {
 
     fun searchTripByDateRange(searchQuery: SearchQuery){
         if (tripList.isNotEmpty()){
-            val dateRange = searchQuery.dateRange
-            if (dateRange?.size != 2){
+            val dateRangeList = searchQuery.dateRange
+            //Assuming only 2 dates would be there
+            if (dateRangeList?.size != 2){
                 throw Exception("Date range is not valid")
             } else{
-                val startDate = dateRange.get(0)
-                val endDate = dateRange.get(1)
+                val startDate = dateRangeList.get(0)
+                val endDate = dateRangeList.get(1)
 
                 val searchResults = tripList.filter {
                     it.startDate in startDate..endDate
                 }
-                println("Search results for dateRange = $dateRange are trips = $searchResults")
+                println("Search results for dateRange = $dateRangeList are trips = $searchResults")
             }
         } else{
             throw Exception("No trips created yet!")
@@ -91,8 +81,9 @@ class TripRepository {
 
         val idxOf = tripList.indexOfFirst { it.id == id }
 
-        if (trip!=null){
+        if (trip != null) {
             val newTripData = trip.copy(isFavouriteTrip = true)
+            //updating the list at that idx after changing the value of isFavouriteTrip
             tripList.set(idxOf, newTripData)
         }
     }
@@ -113,5 +104,17 @@ class TripRepository {
     fun getFavouriteTrips() : List<TripDetails>{
         val favTrips =  tripList.filter { it.isFavouriteTrip }
         return favTrips
+    }
+
+    fun deleteTrip(id: Int){
+        val tripData = tripList.find { it.id == id }
+
+        if (tripList.isNotEmpty()){
+            val isSuccess = tripList.remove(tripData)
+            println("Trip Deletion Success: $isSuccess")
+        }
+        else{
+            throw Exception("No trips exists")
+        }
     }
 }
